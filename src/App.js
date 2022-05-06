6acadbc7722dd1a0f2d3863f82e4d9735a8be6d3
himalbaskota <himalbaskota@gmail.com>
2022-05-06 10:48:07 +0545

    todo_app redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Todo from "./components/Todo";
-const App = () => {
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
   return (
-    <>
-      <Todo />
-    </>
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <TodoList />
+    </div>
   );
-};
+}
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,62 +1,46 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addTodo, deleteTodo, removeTodo } from "../actions/Index";
-import "./Todo.css";
-const Todo = () => {
-  const [inputData, setInputData] = useState("");
-  const list = useSelector((state) => state.todoReducers.list);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <div className="main-div">
-        <div className="child-div">
-          <figure>
-            <figcaption>Add list </figcaption>
-          </figure>
+import TodoForm from "./TodoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
 
-          <div className="addItems">
-            <input
-              type="text"
-              placeholder="add items ..."
-              value={inputData}
-              onChange={(event) => setInputData(event.target.value)}
-            />
-            <i
-              className="fa fa-fa-plus add-btn"
-              onClick={() => dispatch(addTodo(inputData), setInputData(""))}
-            ></i>
-          </div>
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
 
-          <div className="showItems">
-            {list.map((elem) => {
-              return (
-                <div className="eachItem" key={elem.id}>
-                  <h3>{elem.data}</h3>
-                  <div className="todo-btn">
-                    <i
-                      className="far fa-trash-alt add-btn"
-                      title="Delete items"
-                      onClick={() => dispatch(deleteTodo(elem.id))}
-                    ></i>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
 
-          <div className="showItems">
-            <button
-              className="btn effect04"
-              data-sm-link-text="remove All"
-              onClick={() => dispatch(removeTodo())}
-            >
-              <span>check list</span>
-            </button>
-          </div>
-        </div>
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
       </div>
     </div>
-  );
+  ));
 };
 
 export default Todo;
